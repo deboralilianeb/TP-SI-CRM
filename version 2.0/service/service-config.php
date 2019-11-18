54844ebedc8a207b0f4ceb09ac015dc3480bb07b
PauloABM <paulobmatos17@gmail.com>
2019-11-18 14:02:52 -0300

    Inclusão do Service
    Inclusão do primeiro service.

---
+++ version 2.0/service/service-config.php
@@ -0,0 +1,30 @@
+<?php
+# definicções básicas sobre base de dados
+$_config['app_name'] = 'Service CRM';
+$_config['url']      = 'http://localhost/crm/service/';
+ 
+# apresentar o debug completo ?
+$_config['show_error_log'] = false; 
+# -------------------------------------------------------
+/**
+ * Retorna uma resposta JSON para o Solicitante.
+ * 
+ * @param  boolean $__success - indica se houve sucesso na operação
+ * @param  string  $__message - uma mensagem opcional
+ * @param  array   $_dados    - dados a serem retornados
+ * @return JSON REST
+ */
+function __output_header__( $__success = true, $__message = null, $_dados = array() )
+{
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(
+        array(
+            'success' => $__success,
+            'message' => $__message,
+            'dados'   => $_dados
+        )
+    );
+    # por ser a ultima funcao, podemos matar o processo aqui.
+    exit;
+}
+?>
